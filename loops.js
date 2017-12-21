ac6d64c8a6f6786209e38285d70fd7645ff79d29
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-12-21 01:52:11 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    console.log(`"I am ${i} strange loop${i === 0 ? '' : 's'}."`);
+  }
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n);
+  }
+}
